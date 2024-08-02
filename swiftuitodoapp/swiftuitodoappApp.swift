76f285b867133d52d9f462749c7b720cc5e1580d
Rinku Chaudhari <rinku@fetch.ly>
2024-08-02 23:14:17 +0545

    Initial Commit

---
+++ swiftuitodoapp/swiftuitodoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuitodoappApp.swift
+//  swiftuitodoapp
+//
+//  Created by Rinku Chaudhari on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftuitodoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
